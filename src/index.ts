2ef15f0e96a2fa3ee08583b99ea49011fbcb9fe6
shenchao <shenchao@btctrade.com>
2019-09-23 17:41:49 +0800

    同步代码

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,71 @@
-let suits = ["hearts", "spades", "clubs", "diamonds"]
+// function demo1 (arg: string): string 
+// function demo1 (arg: number):number
+// function demo1 (arg): any {
+//   return arg
+// }
 
-function pickCard(x: {suit: string; card: number; }[]): number;
-function pickCard(x: number): {suit: string; card: number; };
-function pickCard(x: any): any {
-    // Check to see if we're working with an object/array
-    // if so, they gave us the deck and we'll pick the card
-    if (typeof x == "object") {
-        let pickedCard = Math.floor(Math.random() * x.length);
-        return pickedCard;
-    }
-    // Otherwise just let them pick the card
-    else if (typeof x == "number") {
-        let pickedSuit = Math.floor(x / 13);
-        return { suit: suits[pickedSuit], card: x % 13 };
-    }
+function demo1 <U>(arg: U): U {
+	return arg
 }
+
+interface Demo2 {
+	<U>(arg: U): U
+}
+
+interface Demo3<D> {
+	(arg: D): D
+}
+
+let demo2: Demo2 = function<T>(arg: T): T {
+	return arg
+}
+
+class Demo<U> {
+	name: string
+	sayHello: (theName: U) => U 
+}
+
+let d = new Demo<string>()
+d.name = 'w3ctalk'
+d.sayHello = function (theName) {
+	return 'hello ' + theName
+}
+
+function identity <T>(arg: T[]): T[] {
+	console.log(arg.length)
+	return arg
+}
+
+let returnNum = identity(['ss', 'ss'])
+
+let identityOwn: <U>(arg: U[]) => U[] = identity
+
+interface Lengthwise {
+  length: number
+}
+
+function loggingIndentity <T extends Lengthwise>(arg: T): T {
+	console.log(arg.length)
+	return arg
+}
+
+// arg参数要有length.
+// loggingIndentity(2)
+
+loggingIndentity({
+    length: 30,
+    name: 'w3ctalk'
+})
+
+function getProperty <T, K extends keyof T>(obj: T, key: K) {
+    return obj[key]
+}
+
+let x = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+getProperty(x, 'a')
+// getProperty(x, 'd')
